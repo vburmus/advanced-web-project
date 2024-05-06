396a10d0c05c093453d664f30e71fdc349611dfd
vburmus <burmusvolod@gmail.com>
2024-05-06 18:34:29 +0200

    feat(app) initial server commit

---
+++ server/src/main/java/com/wust/advanced/web/FleetAssistantApplication.java
@@ -0,0 +1,11 @@
+package com.wust.advanced.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetAssistantApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FleetAssistantApplication.class, args);
+	}
+}
