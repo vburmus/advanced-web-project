565148460029c3445dd7c7bfc497b76c6b71aa3e
vburmus <burmusvolod@gmail.com>
2024-05-13 16:18:59 +0200

    fix(workflow) testing gradle issue
    Co-authored-by: Maryush Padhol <maryushp@gmail.com>

--- server/src/test/java/com/wust/advanced/web/FleetAssistantApplicationTests.java
+++ server/src/test/java/com/wust/advanced/web/FleetAssistantApplicationTests.java
@@ -3,14 +3,9 @@ package com.wust.advanced.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 @SpringBootTest
-class WorkflowTest {
-
-	@Test
-	void workflowTest() {
-		assertEquals(4, 2+2);
-	}
-
+class FleetAssistantApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
