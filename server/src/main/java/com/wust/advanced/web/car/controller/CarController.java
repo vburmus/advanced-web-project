b3b26e2178fa11f76e178cd8e43f250ce5efaa4d
vburmus <burmusvolod@gmail.com>
2024-05-17 18:41:50 +0200

    feat(car) added cars functionality

--- server/src/main/java/com/wust/advanced/web/car/controller/CarController.java
+++ server/src/main/java/com/wust/advanced/web/car/controller/CarController.java
@@ -1,12 +1,17 @@
 package com.wust.advanced.web.car.controller;
 
+import com.wust.advanced.web.car.service.CarService;
 import com.wust.advanced.web.location.service.LocationService;
+import com.wust.advanced.web.utils.dto.CarDto;
 import com.wust.advanced.web.utils.dto.LocationDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -18,6 +23,29 @@ import java.time.Duration;
 @RequiredArgsConstructor
 public class CarController {
     private final LocationService locationService;
+    private final CarService carService;
+
+    @PostMapping
+    public ResponseEntity<CarDto> create(@RequestBody @Valid CarDto carDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .body(carService.create(carDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<CarDto>> readAll(Pageable pageable) {
+        return ResponseEntity.ok(carService.readAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CarDto> readById(@PathVariable Long id) {
+        return ResponseEntity.ok(carService.readById(id));
+    }
+
+    @PostMapping("/{id}/location")
+    public ResponseEntity<LocationDto> updateLocation(@RequestBody @Valid LocationDto locationDTO,
+                                                      @PathVariable Long id) {
+        return ResponseEntity.ok(carService.updateLocation(locationDTO, id));
+    }
     @GetMapping(path = "/{id}/locations", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ServerSentEvent<Page<LocationDto>>> locationStream(@PathVariable Long id,
                                                                    @PageableDefault Pageable pageable) {
@@ -37,5 +65,10 @@ public class CarController {
                                                                        });
 
         return Flux.concat(initialData, periodicUpdates);
+    }
+
+    @GetMapping("/{id}/assign-driver/{driverId}")
+    public ResponseEntity<CarDto> assignDriver(@PathVariable Long id, @PathVariable Long driverId) {
+        return ResponseEntity.ok(carService.assignDriver(id, driverId));
     }
 }
---
+++ server/src/main/java/com/wust/advanced/web/utils/exceptions/ItemNotFoundException.java
@@ -0,0 +1,7 @@
+package com.wust.advanced.web.utils.exceptions;
+
+public class ItemNotFoundException extends RuntimeException{
+    public ItemNotFoundException(String message){
+        super(message);
+    }
+}
