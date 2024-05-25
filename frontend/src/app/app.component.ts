077941c9be3b845f81b234728f8575716f5078f7
maryushp <maryushp@gmail.com>
2024-05-25 14:36:27 +0200

    feat(all-cars) implemented component for all cars page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {AllCarsComponent} from "./all-cars/all-cars.component";
+import {VtNavbarComponent} from "./common/vt-navbar/vt-navbar.component";
 
 @Component({
   selector: 'vt-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AllCarsComponent, VtNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
