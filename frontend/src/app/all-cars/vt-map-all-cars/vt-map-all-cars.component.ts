7829bed66a7f8a4d03aabae1bf96b4d74aef81d8
maryushp <maryushp@gmail.com>
2024-06-03 17:40:45 +0200

    fix(map) changed map initial params

--- frontend/src/app/all-cars/vt-map-all-cars/vt-map-all-cars.component.ts
+++ frontend/src/app/all-cars/vt-map-all-cars/vt-map-all-cars.component.ts
@@ -1,56 +1,53 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {GoogleMap, MapMarker} from '@angular/google-maps';
 import {Car, Location} from "../../../utils/types";
 import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
-  selector: 'vt-map-all-cars',
-  standalone: true,
-  imports: [
-    GoogleMap,
-    MapMarker,
-    NgForOf,
-    NgIf
-  ],
-  templateUrl: './vt-map-all-cars.component.html',
-  styleUrl: './vt-map-all-cars.component.scss'
+    selector: 'vt-map-all-cars',
+    standalone: true,
+    imports: [
+        GoogleMap,
+        MapMarker,
+        NgForOf,
+        NgIf
+    ],
+    templateUrl: './vt-map-all-cars.component.html',
+    styleUrl: './vt-map-all-cars.component.scss'
 })
-export class VtMapAllCarsComponent implements OnInit {
-  @Input() cars!: Car[];
+export class VtMapAllCarsComponent {
+    @Input() cars!: Car[];
 
-  clickedMarkerTitle: string | null = null;
-  center: google.maps.LatLngLiteral = { lat: 51.110754, lng: 17.058006 };
-  zoom = 15;
+    clickedMarkerTitle: string | null = null;
+    center: google.maps.LatLngLiteral = {lat: 51.110754, lng: 17.058006};
+    zoom = 15;
 
-  mapOptions: google.maps.MapOptions = {
-    zoomControl: false,
-    scrollwheel: true,
-    disableDoubleClickZoom: true,
-    maxZoom: 18,
-    minZoom: 13,
-    streetViewControl: false,
-    mapTypeControl: false
-  }
+    mapOptions: google.maps.MapOptions = {
+        zoomControl: true,
+        scrollwheel: false,
+        disableDoubleClickZoom: true,
+        maxZoom: 18,
+        minZoom: 13,
+        streetViewControl: false,
+        mapTypeControl: false
+    };
 
-  carIcon = {
-    url: 'assets/car-icon.png'
-  };
+    carIcon = {
+        url: 'assets/car-icon.png'
+    };
 
-  ngOnInit(): void {
-  }
+    getMarkers(): { position: google.maps.LatLngLiteral, title: string }[] {
+        return this.cars.map(car => {
+            const lastLocation: Location = car.locations[car.locations.length - 1];
+            return {
+                position: {lat: lastLocation.latitude, lng: lastLocation.longitude},
+                title: `Car ${car.id}`
+            };
+        });
+    }
 
-  getMarkers(): { position: google.maps.LatLngLiteral, title: string }[] {
-    return this.cars.map(car => {
-      const lastLocation: Location = car.locations[car.locations.length - 1];
-      return {
-        position: { lat: lastLocation.latitude, lng: lastLocation.longitude },
-        title: `Car ${car.id}`
-      };
-    });
-  }
-
-  markerClicked(title: string) {
-    this.clickedMarkerTitle = title;
-  }
+    markerClicked(title: string) {
+        this.clickedMarkerTitle = title;
+    }
 
 }
