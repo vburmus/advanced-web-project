412978b55684a7226b6771aaf438922814d7d88d
maryushp <maryushp@gmail.com>
2024-06-06 19:09:57 +0200

    fix(routing) fixed routing

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,13 @@ import {bootstrapApplication} from '@angular/platform-browser';
 import {AppComponent} from './app/app.component';
 import {provideHttpClient} from "@angular/common/http";
 import {provideOAuthClient} from "angular-oauth2-oidc";
+import {provideRouter} from "@angular/router";
+import {routes} from "./app/app.routes";
+
 bootstrapApplication(AppComponent, {
     providers: [
         provideHttpClient(),
-        provideOAuthClient()
+        provideOAuthClient(),
+        provideRouter(routes)
     ]
 })    .catch((err) => console.error(err));
