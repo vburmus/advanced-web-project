c9f71eb167d883181dcfc5574c2983102a074a34
vburmus <burmusvolod@gmail.com>
2024-06-06 14:40:17 +0200

    fix(config) angular config

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import {appConfig} from "./app/app.config";
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import {bootstrapApplication} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import {provideHttpClient} from "@angular/common/http";
+import {provideOAuthClient} from "angular-oauth2-oidc";
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideHttpClient(),
+        provideOAuthClient()
+    ]
+})    .catch((err) => console.error(err));
