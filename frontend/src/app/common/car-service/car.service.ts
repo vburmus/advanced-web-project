51f0847ecfea30f3df8411bb705b38674b897790
maryushp <maryushp@gmail.com>
2024-06-06 19:14:07 +0200

    feat(api connection) connected frontend to api

---
+++ frontend/src/app/common/car-service/car.service.ts
@@ -0,0 +1,33 @@
+import {map, Observable, of} from "rxjs";
+import {Car} from "../../../utils/types";
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {OAuthService} from "angular-oauth2-oidc";
+import {environment} from "../../../environments/environment";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CarService {
+    private apiUrl = environment.apiUrl;
+
+    constructor(private http: HttpClient,private oAuthService: OAuthService) {}
+
+    getNearbyCars(): Observable<Car[]> {
+        const headers = new HttpHeaders({
+            'Authorization': `Bearer ${this.oAuthService.getIdToken()}`
+        });
+
+        return this.http.get<{ content: Car[] }>(this.apiUrl + '/cars', { headers }).pipe(
+            map(response => response.content)
+        );
+    }
+
+    getCarById(id: string): Observable<Car> {
+        const headers = new HttpHeaders({
+            'Authorization': `Bearer ${this.oAuthService.getIdToken()}`
+        });
+
+        return this.http.get<Car>(this.apiUrl + '/cars/' + id, { headers });
+    }
+}
